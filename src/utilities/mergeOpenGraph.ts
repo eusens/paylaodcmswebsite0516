0ba857d8c36f1a9c162f4824967cd2917ad1cc0f
Steven Qi <stevenqi@StevendeMacBook-Air.local>
2025-06-04 15:56:06 +0800

    eidt site name and title

--- src/utilities/mergeOpenGraph.ts
+++ src/utilities/mergeOpenGraph.ts
@@ -9,8 +9,8 @@ const defaultOpenGraph: Metadata['openGraph'] = {
       url: `${getServerSideURL()}/website-template-OG.webp`,
     },
   ],
-  siteName: 'Payload Website Template',
-  title: 'Payload Website Template',
+  siteName: 'Industrial Automation Components',
+  title: 'Industrial Automation Components',
 }
 
 export const mergeOpenGraph = (og?: Metadata['openGraph']): Metadata['openGraph'] => {
