c02f0e754dd5bccf8c40c3593312c9e6e1fa4e9a
eusens <eusens@qiweiting@outlook.com>
2026-04-08 13:52:49 +0800

    动态页面limit2500

--- src/app/(frontend)/products/[slug]/page.tsx
+++ src/app/(frontend)/products/[slug]/page.tsx
@@ -28,7 +28,7 @@ export async function generateStaticParams() {
   const posts = await payload.find({
     collection: 'posts',
     draft: false,
-    limit: 10000,
+    limit: 2500,
     overrideAccess: false,
     pagination: false,
     select: {
